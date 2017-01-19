2b24ffecb4db0b666b2e19c13d601625223a02af
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2017-01-19 14:11:10 +0900

    fix build issue and updated dependency versions

--- ts/src/context-menu.ts
+++ ts/src/context-menu.ts
@@ -13,7 +13,7 @@
 //    limitations under the License.
 
 /// <reference path="../../node_modules/@types/chrome/index.d.ts" />
-/// <reference path="../../node_modules/@types/moment/index.d.ts" />
+/// <reference path="../../node_modules/moment/moment.d.ts" />
 
 import * as moment from 'moment';
 
